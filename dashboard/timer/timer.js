85b173c8c5e35fd89827279f0c83dec76c34ea7c
rickychann <161703503+mechikacha@users.noreply.github.com>
2024-08-06 03:06:24 +0900

    Added 4-way layout. Also some changes on directory, filename, assets, and more.

--- dashboard/timer/timer.js
+++ dashboard/timer/timer.js
@@ -1,10 +1,19 @@
 const watch = document.querySelector('#watch');
 let milliseconds = 0;
 let timer;
+let isTimerStarted;
 
 const timerReplicant = nodecg.Replicant("timer", {
   defaultValue: "00:00:00"
 });
+
+const isTimerStartedReplicant = nodecg.Replicant("timerflag");
+
+function updateIsTimerStarted(bool) {
+  isTimerStarted = bool;
+  console.log(isTimerStarted)
+  isTimerStartedReplicant.value = isTimerStarted;
+}
 
 function startWatch() {
   watch.classList.remove('paused');
@@ -37,11 +46,12 @@ function resetWatch() {
   milliseconds = 0;
   watch.innerHTML= '00:00:00';
   timerReplicant.value = '00:00:00';
+  updateIsTimerStarted(false)
 };
 
 document.addEventListener('click', (e) =>{
   const el = e.target;
-  if (el.id === 'start') startWatch();
+  if (el.id === 'start') startWatch(), updateIsTimerStarted(true);
   if (el.id === 'pause') pauseWatch();
   if (el.id === 'reset') resetWatch();
 });
--- graphics/timer/index.js
+++ graphics/timer/index.js
@@ -1,3 +1,9 @@
 nodecg.Replicant("timer").on("change", (newValue, oldValue) => {
   document.getElementById("timer").innerText = newValue;
 });
+
+nodecg.Replicant("timerflag").on("change", (newValue, oldValue) => {
+  if (newValue = true) {
+    console.log("Started")
+  }
+});
